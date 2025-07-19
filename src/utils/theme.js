6263857b6b464125aaf60c3eb69a152c884553ab
leealswl <alswl54321600@gmail.com>
2025-07-19 13:59:13 +0900

    main front-end added

---
+++ src/utils/theme.js
@@ -0,0 +1,21 @@
+export const lightTheme = {
+    bgColor: '#FFFFFF',
+    textColor: '#333333',
+    borderColor: '#EEEEEE',
+    accentColor: '#007AFF',
+    // ... 다른 색상이나 스타일 값 추가 가능
+  };
+  
+  export const darkTheme = {
+    bgColor: '#1E1E1E',
+    textColor: '#FFFFFF',
+    borderColor: '#444444',
+    accentColor: '#0A84FF',
+    // ... 다른 색상이나 스타일 값 추가 가능
+  };
+  
+  // 두 테마를 한번에 export하여 사용하기 쉽게 만듭니다.
+  export const theme = {
+    lightTheme,
+    darkTheme,
+  };
